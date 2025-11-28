db346e213727c1f0a8e6a65c0a4a978642ae8d57
HardBoss07 <matteo@bosshard.net>
2025-11-28 13:36:16 +0100

    updated basic.rs

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,15 +1,20 @@
 use minifb::Key;
-use totalfb::{FbWindow, FrameBuffer};
+use totalfb::{FbWindow, FrameBuffer, FbMode};
 
 fn main() {
     let (w, h) = (400, 300);
 
-    let mut fb = FrameBuffer::new(w, h);
-    let mut win = FbWindow::new("totalfb example", w, h);
+    // Normal 1:1 framebuffer
+    let fb = FrameBuffer::with_mode(w, h, FbMode::Normal);
+
+    // Create window with scale=1
+    let mut win = FbWindow::new("totalfb basic example", w, h, fb.mode_scale());
+
+    let mut fb = fb; // mutable after window is created
 
     while win.is_open() && !win.is_key_down(Key::Escape) {
         fb.clear(0x002244);
         fb.set_pixel(100, 100, 0xFFFFFF);
-        win.update(fb.raw());
+        win.update(&fb);
     }
 }
